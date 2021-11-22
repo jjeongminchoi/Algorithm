13bdd50f26c539e0270a7901369930d0eb3b09f5
최정민 <dndbejtk@naver.com>
2021-11-23 00:09:55 +0900

    .

---
+++ src/BOJ/stage01_입출력과_사칙연산/_01_2557.java
@@ -0,0 +1,12 @@
+package BOJ.stage01_입출력과_사칙연산;
+
+// 2557번 Hello World!를 출력하시오.
+
+public class _01_2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// 메모리 : 11208KB
+// 시간 : 72ms
