826258f724d17a3ed13c640ed65ba7c567b6aa54
최정민 <dndbejtk@naver.com>
2021-11-24 00:50:33 +0900

    We love kriii

---
+++ src/BOJ/stage01_입출력과_사칙연산/_02_10718.java
@@ -0,0 +1,13 @@
+package BOJ.stage01_입출력과_사칙연산;
+
+// 10718번 두 줄에 걸쳐 "강한친구 대한육군"을 한 줄에 한 번씩 출력한다.
+
+public class _02_10718 {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
+
+// 메모리 : 11288KB
+// 시간 : 76ms
