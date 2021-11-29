fd96f4b118a2033fbb1403acdeb92f80fc0dadda
최정민 <dndbejtk@naver.com>
2021-11-30 00:38:55 +0900

    개

---
+++ src/BOJ/stage01_입출력과_사칙연산/_04_10172.java
@@ -0,0 +1,16 @@
+package BOJ.stage01_입출력과_사칙연산;
+
+// 10172번 아래 예제와 같이 개를 출력하시오.
+
+public class _04_10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
+
+// 메모리 : 11220 KB
+// 시간 :  72 ms
