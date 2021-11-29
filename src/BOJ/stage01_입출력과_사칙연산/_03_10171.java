4199b4b64722c6edb60cfdad378b9d26589511a5
최정민 <dndbejtk@naver.com>
2021-11-29 23:17:35 +0900

    고양이

---
+++ src/BOJ/stage01_입출력과_사칙연산/_03_10171.java
@@ -0,0 +1,15 @@
+package BOJ.stage01_입출력과_사칙연산;
+
+// 10171번 아래 예제와 같이 고양이를 출력하시오.
+
+public class _03_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
+
+// 메모리 : 11204 KB
+// 시간 :  72 ms
--- src/BOJ/test.java
+++
@@ -1,7 +0,0 @@
-package BOJ;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("test중입니다");
-    }
-}
