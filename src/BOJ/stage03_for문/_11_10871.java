86b55e8826ab33cd73eb821eaf1e0a20fe3042b9
최정민 <dndbejtk@naver.com>
2021-12-25 23:59:00 +0900

    X보다 작은 수 -1

---
+++ src/BOJ/stage03_for문/_11_10871.java
@@ -0,0 +1,26 @@
+package BOJ.stage03_for문;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _11_10871 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder stringBuilder = new StringBuilder();
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int index = Integer.parseInt(stringTokenizer.nextToken());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+
+        int[] A = new int[index];
+
+        for (int i = 0 ; i < A.length ; i++) {
+
+        }
+    }
+}
+
+// 메모리 : 14204 KB
+// 시간 : 128 ms
