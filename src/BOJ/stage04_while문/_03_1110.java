37e975668dcaba5baacfabede2c12f5ffdee5674
최정민 <dndbejtk@naver.com>
2021-12-30 01:13:21 +0900

    더하기 사이클

--- src/BOJ/stage04_while문/_03_1110.java
+++ src/BOJ/stage04_while문/_03_1110.java
@@ -10,21 +10,31 @@ public class _03_1110 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String num = br.readLine(); // 최초 숫자
-        String newNum = null; // 변하는 숫자 & 최종 숫자
+        String temp = num;
+        String resNum = null; // 변하는 숫자 & 최종 숫자
         int count = 0;
-        boolean flag = !Objects.equals(num, newNum);
 
-        while (flag) { // 최초 숫자와 최종 숫자가 같으면 false 로 종료.
-            newNum = num;
-            if (Integer.parseInt(newNum) < 10) {
-                newNum = "0" + newNum;
+        while (!Objects.equals(num, resNum)) { // 최초 숫자와 최종 숫자가 같으면 false 로 종료.
+            if (temp.charAt(0) == '0') {
+                temp = Integer.toString(temp.charAt(1) - 48);
+            } else {
+                if (Integer.parseInt(temp) < 10) {
+                    temp = "0" + temp;
+                }
+                int A = temp.charAt(0) - 48;
+                int B = temp.charAt(1) - 48;
+                int sum = A + B;
+
+                if (sum < 10) {
+                    A = sum;
+                } else {
+                    A = Integer.toString(sum).charAt(1) - 48;
+                }
+                temp = B + Integer.toString(A);
+
+                resNum = temp;
+                count++;
             }
-            int A = newNum.charAt(0);
-            int B = newNum.charAt(1);
-            int sum = A + B;
-            newNum = B + Integer.toString(sum);
-
-            count++;
         }
         System.out.println(count);
     }
