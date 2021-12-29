af25c053b39bc6319a9c0f945b53af5283162603
최정민 <dndbejtk@naver.com>
2021-12-30 01:41:30 +0900

    더하기 사이클

--- src/BOJ/stage04_while문/_03_1110.java
+++ src/BOJ/stage04_while문/_03_1110.java
@@ -3,42 +3,23 @@ package BOJ.stage04_while문;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Objects;
 
 public class _03_1110 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String num = br.readLine(); // 최초 숫자
-        String temp = num;
-        String resNum = null; // 변하는 숫자 & 최종 숫자
+        int N = Integer.parseInt(br.readLine());
+        int copyN = N;
         int count = 0;
 
-        while (!Objects.equals(num, resNum)) { // 최초 숫자와 최종 숫자가 같으면 false 로 종료.
-            if (temp.charAt(0) == '0') {
-                temp = Integer.toString(temp.charAt(1) - 48);
-            } else {
-                if (Integer.parseInt(temp) < 10) {
-                    temp = "0" + temp;
-                }
-                int A = temp.charAt(0) - 48;
-                int B = temp.charAt(1) - 48;
-                int sum = A + B;
+        do {
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+            count++;
+        } while (N != copyN);
 
-                if (sum < 10) {
-                    A = sum;
-                } else {
-                    A = Integer.toString(sum).charAt(1) - 48;
-                }
-                temp = B + Integer.toString(A);
-
-                resNum = temp;
-                count++;
-            }
-        }
         System.out.println(count);
     }
 }
 
-// 메모리 : 14144 KB
-// 시간 : 124 ms
+// 메모리 : 14236 KB
+// 시간 : 120 ms
