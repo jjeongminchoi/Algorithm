bc95edf25c63a0290080e8bd432f4f72edbeb35a
최정민 <dndbejtk@naver.com>
2022-01-04 00:12:10 +0900

    숫자의 개수

--- src/BOJ/stage05_1차원_배열/_03_2577.java
+++ src/BOJ/stage05_1차원_배열/_03_2577.java
@@ -3,13 +3,13 @@ package BOJ.stage05_1차원_배열;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class _03_2577 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder result = new StringBuilder();
 
+        // 입력받은 3개의 숫자 곱하기
         int[] num = new int[3];
         int multiply = 1;
         for (int i = 0 ; i < num.length ; i++) {
@@ -17,26 +17,27 @@ public class _03_2577 {
             multiply *= num[i];
         }
 
-//        String num2 = Integer.toString(multiply);
+        // 곱한 값을 10으로 나눈 나머지를 인덱스로 활용하여 해당 인덱스 +1 카운트 후 10으로 나눈다.
+        // 만약 곱한 값이 0이하라면 카운트를 그만한다.
         int[] numCount = new int[10];
         for (int i = 0 ; i < numCount.length ; i++) {
-//            numCount[num2.charAt(i) - 48]++;
-            if (multiply < 0) {
-                break;
-            } else {
+            if (multiply > 0) {
                 numCount[multiply % 10]++;
-                multiply /= 10;
+            } else {
+                break;
             }
+            multiply /= 10;
         }
 
-        for (int i = 0 ; i < numCount.length ; i++) {
-            result.append(i + " : ").append(numCount[i]).append("번").append("\n");
+        // 결과 출력
+        for (int i : numCount) {
+            result.append(i).append("\n");
         }
+
         System.out.println(result);
-
         br.close();
     }
 }
 
-// 메모리 : 14252 KB
+// 메모리 : 14180 KB
 // 시간 : 124 ms
