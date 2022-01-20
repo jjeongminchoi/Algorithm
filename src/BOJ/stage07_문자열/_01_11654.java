f8bd1571e1c0976b2d090d3f9b75324696a9e176
최정민 <dndbejtk@naver.com>
2022-01-20 20:00:08 +0900

    아스키 코드

---
+++ src/BOJ/stage07_문자열/_01_11654.java
@@ -0,0 +1,21 @@
+package BOJ.stage07_문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _01_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int result = (int) br.readLine().charAt(0);
+        System.out.println(result);
+
+//        방법 2)
+//        int input = System.in.read();
+//        System.out.println(input);
+    }
+}
+
+// 메모리 : 14356 KB
+// 시간 : 128 ms
