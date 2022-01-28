e8b9afceb6840d39241d04956566d38a2ada1ff0
최정민 <dndbejtk@naver.com>
2022-01-29 00:38:43 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ src/BOJ/stage01_입출력과_사칙연산/_13_18108.java
@@ -0,0 +1,15 @@
+package BOJ.stage01_입출력과_사칙연산;
+
+import java.util.Scanner;
+
+public class _13_18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int changeYear = 543;
+        System.out.println(sc.nextInt() - changeYear);
+        sc.close();
+    }
+}
+
+// 메모리 : 17696 KB
+// 시간 : 228 ms
