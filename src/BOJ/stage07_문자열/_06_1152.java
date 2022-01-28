293b6564e300b1d88f24ea72053eeb2eb124c989
최정민 <dndbejtk@naver.com>
2022-01-28 23:19:54 +0900

    단어의 개수

--- src/BOJ/stage07_문자열/_06_1152.java
+++ src/BOJ/stage07_문자열/_06_1152.java
@@ -1,10 +1,18 @@
 package BOJ.stage07_문자열;
 
-public class _06_1152 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class _06_1152 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer word = new StringTokenizer(br.readLine().trim(), " ");
+        System.out.println(word.countTokens());
+        br.close();
     }
 }
 
-// 메모리 :  KB
-// 시간 :  ms
+// 메모리 : 20016 KB
+// 시간 : 272 ms
