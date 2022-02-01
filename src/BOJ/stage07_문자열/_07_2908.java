bc1e47da943a49ee788780f1aa1230520bc7d21b
최정민 <dndbejtk@naver.com>
2022-02-01 22:32:25 +0900

    상수

---
+++ src/BOJ/stage07_문자열/_07_2908.java
@@ -0,0 +1,28 @@
+package BOJ.stage07_문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _07_2908 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer inputNum = new StringTokenizer(br.readLine());
+
+        StringBuilder numA = new StringBuilder(inputNum.nextToken());
+        StringBuilder numB = new StringBuilder(inputNum.nextToken());
+
+        int resA = Integer.parseInt(numA.reverse().toString());
+        int resB = Integer.parseInt(numB.reverse().toString());
+
+        if (resA > resB) {
+            System.out.println(resA);
+        } else {
+            System.out.println(resB);
+        }
+    }
+}
+
+// 메모리 : 14200 KB
+// 시간 : 120 ms
