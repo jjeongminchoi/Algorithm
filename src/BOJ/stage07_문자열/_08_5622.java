2d66855f2b9e17cf1b09f6432890bcaadeceb947
최정민 <dndbejtk@naver.com>
2022-02-04 23:58:55 +0900

    다이얼

---
+++ src/BOJ/stage07_문자열/_08_5622.java
@@ -0,0 +1,22 @@
+package BOJ.stage07_문자열;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _08_5622 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int inputLen = br.readLine().length();
+
+        if (2 <= inputLen && inputLen <= 15) {
+            String[] input = br.readLine().split("");
+
+
+        }
+    }
+}
+
+// 메모리 :  KB
+// 시간 :  ms
